2a4beb1b24fb01a44600e134e4dc5815da56722e
[dyad] <agenciastudio4x@gmail.com>
2026-02-10 14:07:41 -0300

    [dyad] Corrigindo erro 404 no acesso ao Admin - wrote 1 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "1.8.5";
+  const version = "1.8.6";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

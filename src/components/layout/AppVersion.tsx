95e9e0c3d35e47dcd3ca4ed8d218cea2c7c031f2
[dyad] <agenciastudio4x@gmail.com>
2026-02-10 09:29:54 -0300

    [dyad] Certificado disponibilizado no dashboard e página do curso. - wrote 3 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "1.5.9";
+  const version = "1.6.0";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

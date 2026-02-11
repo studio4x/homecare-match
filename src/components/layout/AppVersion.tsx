60db648b8370c5bb354465cdf98846b6ca726297
[dyad] <agenciastudio4x@gmail.com>
2026-02-11 17:12:18 -0300

    [dyad] Orientação sobre IDs de preço, Webhooks e ativação do modo Stripe - wrote 1 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "2.2.5";
+  const version = "2.2.6";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

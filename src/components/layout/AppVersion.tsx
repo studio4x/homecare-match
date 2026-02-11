2fd30ca70e01da107efd66bbe724cbfdff64f3bd
[dyad] <agenciastudio4x@gmail.com>
2026-02-11 17:50:52 -0300

    [dyad] Atualização da versão do build para 2.3.4 - wrote 1 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "2.3.3";
+  const version = "2.3.4";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

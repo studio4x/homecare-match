2d438894d54a0e1ee5c7652c567e4a86dbba86f6
[dyad] <agenciastudio4x@gmail.com>
2026-02-12 18:54:50 -0300

    [dyad] Confirmação de sucesso na integração Stripe - wrote 1 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "2.8.6";
+  const version = "2.8.7";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

2a40f8266412e59a412f050f23d26b7da7855a22
[dyad] <agenciastudio4x@gmail.com>
2026-02-17 16:03:12 -0300

    [dyad] Corrigindo erro de texto no modal do mapa - wrote 2 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "4.1.9";
+  const version = "4.2.0";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

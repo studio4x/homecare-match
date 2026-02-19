dcb87d2edcd03dcfb23ded115c1dc8bb518a7d53
[dyad] <agenciastudio4x@gmail.com>
2026-02-19 15:35:45 -0300

    [dyad] Removendo fundo indesejado nas notificações push - wrote 2 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "4.9.3";
+  const version = "4.9.4";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

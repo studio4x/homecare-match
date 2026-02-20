f22c61a162a61ec79ec1346ee7567dd992f16c00
[dyad] <agenciastudio4x@gmail.com>
2026-02-20 16:14:22 -0300

    [dyad] Correção de colunas na tabela feature_videos - wrote 1 file(s), executed 1 SQL queries

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "5.8.13"; // Incremented version
+  const version = "5.8.14"; // Incremented version
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

e1ae7ec6a7ac5e45837c94cd1aaa4b3916da76b0
[dyad] <agenciastudio4x@gmail.com>
2026-02-20 09:19:42 -0300

    [dyad] Corrigido corte lateral da notificação push no mobile - wrote 2 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "5.2.4";
+  const version = "5.2.5";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

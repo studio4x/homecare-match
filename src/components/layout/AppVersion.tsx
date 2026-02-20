00a4c83347d00577a9767e01187281920b8fab6b
[dyad] <agenciastudio4x@gmail.com>
2026-02-20 09:39:40 -0300

    [dyad] Remoção do backdrop das notificações push - wrote 2 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "5.2.8";
+  const version = "5.2.9";
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

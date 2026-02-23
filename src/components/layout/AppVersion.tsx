df6cbe227f4b94981e7ffd9cb3a7af25641769f5
[dyad] <agenciastudio4x@gmail.com>
2026-02-23 17:39:52 -0300

    [dyad] Correção de permissões de leitura (SELECT) em denúncias - wrote 1 file(s), executed 1 SQL queries

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "5.8.79"; // Versão incrementada
+  const version = "5.8.80"; // Versão incrementada
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (

a70e4babba9db52ecdbbdfc0c9a0612c3b4acd8d
[dyad] <agenciastudio4x@gmail.com>
2026-02-23 17:12:34 -0300

    [dyad] Atualizada a versão do build - wrote 1 file(s)

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const AppVersion = () => {
-  const version = "5.8.75"; // Versão incrementada
+  const version = "5.8.76"; // Versão incrementada
   const lastUpdate = new Date().toLocaleDateString('pt-BR');
 
   return (
